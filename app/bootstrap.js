1eb833aec73f74719e2eb3eb600630c2814797d4
陳約銘 <chenyueming@chenyuemingde-MacBook-Pro.local>
2017-12-22 15:43:30 +0800

    ball game

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,12 +1,153 @@
 window.onload = () => {
+  var ballRadius = 10;
+  var dx = 2;
+  var dy = -2;
+  var canvas = document.getElementById('tutorial');
+  var ctx = canvas.getContext('2d');
+  var rightPressed = false;
+  var leftPressed = false;
+  var paddleHeight = 10;
+  var paddleWidth = 75;
+  var paddleX = (canvas.width - paddleWidth) / 2;
+  var brickRowCount = 3;
+  var brickColumnCount = 5;
+  var brickWidth = 75;
+  var brickHeight = 20;
+  var brickPadding = 10;
+  var brickOffsetTop = 30;
+  var brickOffsetLeft = 30;
+
+  var x = canvas.width / 2;
+  var y = canvas.height - 30;
+  var bricks = [];
+  for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+      bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+  }
+  function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+      for (r = 0; r < brickRowCount; r++) {
+        if (bricks[c][r].status == 1) {
+          var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+          var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+
+  function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+      for (r = 0; r < brickRowCount; r++) {
+        var b = bricks[c][r];
+        if (b.status == 1) {
+          if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+
+            dy = -dy;
+            b.status = 0;
+          }
+        }
+      }
+    }
+  }
 
 
+  function drawPaddle() {
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+      paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+    x += dx;
+    y += dy;
+    if (y + dy < ballRadius) {
+      dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+      if (x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy;
+      }
+      else {
+        // alert("GAME OVER");
+        // document.location.reload();
+      }
+    }
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+    }
+
+
+    drawBricks();
+    drawPaddle();
+    collisionDetection();
+  }
+  setInterval(draw, 10);
+
+  function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+      rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+      rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+      leftPressed = false;
+    }
+  }
+  function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+      paddleX = relativeX - paddleWidth / 2;
+    }
+  }
+
+  document.addEventListener("mousemove", mouseMoveHandler, false);
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
 }
+const keyHook = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
 
 class controller {
 
-  constructor() {
 
+  constructor() {
+    // document.addEventListener('keyup', (e) => {
+    //   if (keyHook.indexOf(e.key) !== -1) {
+    //     console.log('move');
+    //   }
+    // })
+    var canvas = document.getElementById('tutorial');
+    var ctx = canvas.getContext('2d');
   }
 
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
+      index: './demo/demo.html',
       baseDir: "./"
     }
   });
-  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("app/style/*.scss", ['sass']);
   gulp.watch("app/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
-  return gulp.src("app/style/*.scss")
+  return gulp.src("app/style/**.scss")
     .pipe(sass())
     .pipe(gulp.dest("app/css"))
     .pipe(browserSync.stream());
 });
 
 
-
-
-gulp.task('default', function () {
-  // place code for your default task here
-});
+gulp.task('default', ['browser-sync']);
