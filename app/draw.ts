e79187ea6de7bf486eb1262f5d0257ec5958811e
yueming-Chen <aa1235561@gmail.com>
2018-03-03 20:44:12 +0800

    1.支援圖片固定

--- app/draw.ts
+++ app/draw.ts
@@ -1,3 +1,9 @@
+
+export interface Image {
+  path: string;
+  posX: number | string;
+  posY: number | string;
+}
 export class drawKernal {
   // 重新繪圖頻率
   // 重新繪圖主程式
@@ -6,13 +12,17 @@ export class drawKernal {
   private _drawMain = setInterval(() => { }, this._drawHZ);
 
   private _registList: any[] = [];
+
   private refreshms = 10;
+
+  private imageBaseUrl = '/images/';
+
+  private _Images: Image[] = [];
 
   constructor() { }
 
   run() {
-
-    this._drawMain = setInterval(this.register, this.refreshms);
+    this._drawMain = setInterval(this.draw, this.refreshms);
   }
 
   stop() {
@@ -24,12 +34,20 @@ export class drawKernal {
     if (this._registList.indexOf(cb) === -1) this._registList.push(cb);
   }
 
-  register() {
-
+  draw() {
+    this._Images.map(v => {
+      let { path, posX, posY } = v;
+      path = this.imageBaseUrl + path;
+      // canvas paint on (path,posX,posY)
+    });
   }
 
   removeRegister() {
     this._registList.map(v => { return v });
+  }
+
+  addImages(image: Image) {
+    this._Images.push(image);
   }
 
 }
